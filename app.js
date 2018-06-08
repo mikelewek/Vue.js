0abdef1478c308c485ee122ef692d6526b0ca5d2
Mike Lewek <mlewek@raysbaseball.com>
2018-06-08 13:34:45 -0400

    Update js for query by param, css, html

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-const ApiUrl = '..//';
-
 const app = new Vue({
 	el:'#app',
 	data:{
 		pvIdList:'',
 		results:[],
 		noResults:false,
-		searching:false
+        searching: false,
+        searchBy: 'pvid',
+        errorMessage: '',
 	},
 	methods:{
         multiSearch: function () {
+            this.results = [];
+            var apiUrl = '';
+
             if (this.pvIdList.length === 0) {
                 this.searching = false;
                 this.noResults = true;
                 return;
             }
 
+            switch (this.searchBy) {
+                case 'pvid':
+                    apiUrl = '../api/crm/contact/getbypvid/';
+                    break;
+                case 'contactid':
+                    apiUrl = '../api/crm/contact/';
+                    break;
+                case 'email':
+                    apiUrl = '../api/crm/contact/getbyemail/';
+                    break;
+                case 'lastname':
+                    apiUrl = '../api/crm/contact/getbylastname/';
+                    break;
+                case 'fullname':
+                    apiUrl = '../api/crm/contact/getbyfullname/';
+                    break;
+            }
+
             var json = this.pvIdList.split("\n");
 
-            this.$http.post(ApiUrl, json)
+            this.$http.post(apiUrl, json)
                 .then(function (res) {
                     this.searching = false;
                     this.noResults = false;
@@ -26,6 +47,8 @@ const app = new Vue({
 
                 }, function (response) {
                     // Error
+                    this.noResults = true;
+                    this.errorMessage = ' - Error. It\'s possible there were too many results or and invalid entry causing the error.';
                     console.log(res)
                 });
 		}
